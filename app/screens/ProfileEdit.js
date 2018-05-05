b3b47ec93545e7e812ef7be2482f208b141a69a2
hafizjoundy <hafizjoundys@gmail.com>
2018-05-05 12:42:12 +0700

    wahid3

--- app/screens/ProfileEdit.js
+++ app/screens/ProfileEdit.js
@@ -3,31 +3,36 @@ import {connect} from 'react-redux'
 
 class ProfileEdit extends Component {
     render() {
+
+        state = {
+            data :{}
+        }
+
+        this.setState({
+            data = this.props.navigation.state.params.data
+        })
+
         return (
             <Form>
                 <Item inlineLabel>
                     <Label>Name</Label>
-                    <Input/>
+                    <Input value={this.state.data.name}/>
                 </Item>
                 <Item inlineLabel last>
                     <Label>Department</Label>
-                    <Input/>
+                    <Input value={this.state.data.department}/>
                 </Item>
                 <Item inlineLabel last>
                     <Label>University</Label>
-                    <Input/>
-                </Item>
-                <Item inlineLabel last>
-                    <Label>Department</Label>
-                    <Input/>
+                    <Input value={this.state.data.university}/>
                 </Item>
                 <Item inlineLabel last>
                     <Label>From</Label>
-                    <Input/>
+                    <Input value={this.state.data.from}/>
                 </Item>
                 <Item inlineLabel last>
                     <Label>Higlights</Label>
-                    <Input/>
+                    <Input value={this.state.data.highlight}/>
                 </Item>
             </Form>
         )
